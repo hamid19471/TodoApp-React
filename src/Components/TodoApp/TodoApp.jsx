1c89849186c05dea3ff9acf49923ac55f5217662
Hamid Asadi <hamid.19471@gmail.com>
2022-10-31 21:24:10 +0330

    Refactor component and add to app.js

--- src/Components/TodoApp/TodoApp.jsx
+++ src/Components/TodoApp/TodoApp.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import TodoForm from "../TodoForm/TodoForm";
+import { Typography } from "@mui/material";
 
 const TodoApp = () => {
-  return <div>TodoApp</div>;
+  return (
+    <Grid
+      container
+      justifyContent={"center"}
+      alignContent={"center"}
+      flexDirection={"column"}>
+      <Typography variant='h1' mb={5}>
+        Todo-pplication
+        <Typography textAlign={"center"} borderTop={1} pt={1}>
+          APP FOR MANAGE YOU WORK AND TASKS{" "}
+        </Typography>
+      </Typography>
+      <TodoForm />
+    </Grid>
+  );
 };
 
 export default TodoApp;
