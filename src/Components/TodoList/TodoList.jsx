64b6e1e5f1a33dd5ec9f0be3a8ec168cf6aeb661
Hamid Asadi <hamid.19471@gmail.com>
2022-10-31 21:51:05 +0330

    add styles for component

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,7 +1,26 @@
+import Box from "@mui/material/Box";
 import React from "react";
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+  return (
+    <Box
+      bgcolor={"#f4f4f4"}
+      alignItems={"center"}
+      display={"flex"}
+      justifyContent={"center"}
+      mt={5}
+      borderRadius={2}
+      fontSize={22}
+      border={1}>
+      <Box
+        justifyContent={"center"}
+        alignItems={"center"}
+        display={"flex"}
+        p={2}>
+        TodoList
+      </Box>
+    </Box>
+  );
 };
 
 export default TodoList;
