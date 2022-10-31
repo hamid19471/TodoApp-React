742d78e29287f748cc932cd1276d33c190608753
Hamid Asadi <hamid.19471@gmail.com>
2022-10-31 21:50:39 +0330

    Add submit and value handler for form

--- src/Components/TodoForm/TodoForm.jsx
+++ src/Components/TodoForm/TodoForm.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 
 const TodoForm = () => {
+  const [todo, setTodo] = useState("");
+  const onChangeHandler = (event) => {
+    setTodo(event.target.value);
+    console.log(event.target.value);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
   return (
-    <Box alignItems={"center"} display={"flex"} gap={2}>
-      <TextField label='ADD TODOS HERE...' variant='filled' fullWidth />
-      <Button variant='contained' sx={{ padding: "4px" }}>
+    <Box
+      alignItems={"center"}
+      display={"flex"}
+      gap={2}
+      component='form'
+      onSubmit={onSubmitHandler}>
+      <TextField
+        label='ADD TODOS HERE...'
+        variant='filled'
+        value={todo}
+        onChange={onChangeHandler}
+        fullWidth
+      />
+      <Button variant='contained' sx={{ padding: "4px" }} type={"submit"}>
         ADD TODO
       </Button>
     </Box>
