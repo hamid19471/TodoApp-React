44470aad0d0c75cceedc899a4a1d32643bb73b0b
Hamid Asadi <hamid.19471@gmail.com>
2022-10-31 21:24:41 +0330

    add TodoApp component here and refactor main app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import { Grid, Typography } from "@mui/material";
-import TodoForm from "./Components/TodoForm/TodoForm";
+import TodoApp from "./Components/TodoApp/TodoApp";
+
 function App() {
   return (
-    <Grid
-      container
-      justifyContent={"center"}
-      alignContent={"center"}
-      flexDirection={"column"}>
-      <Typography variant='h1' mb={5}>
-        Todo-pplication
-        <Typography textAlign={"center"} borderTop={1} pt={1}>
-          APP FOR MANAGE YOU WORK AND TASKS{" "}
-        </Typography>
-      </Typography>
-      <TodoForm /> {/* FormComponent importe */}
-    </Grid>
+    <>
+      <TodoApp />
+    </>
   );
 }
 
